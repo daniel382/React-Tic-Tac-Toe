9fec4969d09afbb7ca849e1cb9f0f092305a1b54
Daniel Lucas <daniellucas382@gmail.com>
2019-04-11 15:55:10 -0300

    Lifting state up

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,7 @@
-class Square extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = { value: null }
-    }
-
-    render() {
-        /* 
-            Additional Reading:
-            https://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
-        */
-        return (
-            <button className="square" onClick={ () => this.setState({ value: 'X' }) } >
-                { this.state.value }
-            </button>
-        )
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={ props.onClick } >
+            { props.value }
+        </button>
+    )
 }
