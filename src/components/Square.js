59b810320203a9967c6af2317141cc1d1a32f1fe
Daniel Lucas <daniellucas382@gmail.com>
2019-04-11 15:39:19 -0300

    Using state

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,18 @@
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { value: null }
+    }
+
     render() {
         /* 
-            props is how information flows in React Apps from parents to children
+            Additional Reading:
+            https://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
         */
         return (
-            <button className="square">
-                { this.props.value }
+            <button className="square" onClick={ () => this.setState({ value: 'X' }) } >
+                { this.state.value }
             </button>
         )
     }
